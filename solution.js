41c233617a094724548680c9a440dbca27d18b6a
ravichandrapalla <cravi6635@gmail.com>
2022-12-27 11:11:43 +0530

    solution done - 1

--- solution.js
+++ solution.js
@@ -1,6 +1,16 @@
 // complete the given function
 
-function palindrome(str){
-
+function palindrome(str) {
+  let newstr = str;
+  let start = 0;
+  let end = newstr.length - 1;
+  while (start < end) {
+    if (newstr.charAt(start) !== newstr.charAt(end)) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+  return true;
 }
-module.exports = palindrome
+module.exports = palindrome;
